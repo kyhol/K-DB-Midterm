d17ff84c08d82145abc25c763873456a77032f57
kyhol <Kyle.Hollett@keyin.com>
2024-10-30 17:47:17 -0230

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { Pool } = require('pg');
-
+const { Pool } = require("pg");
+//Absolute test...
 // PostgreSQL connection
 const pool = new Pool({
-  user: 'postgres', //This _should_ be your username, as it's the default one Postgres uses
-  host: 'localhost',
-  database: 'your_database_name', //This should be changed to reflect your actual database
-  password: 'your_database_password', //This should be changed to reflect the password you used when setting up Postgres
+  user: "postgres", //This _should_ be your username, as it's the default one Postgres uses
+  host: "localhost",
+  database: "your_database_name", //This should be changed to reflect your actual database
+  password: "your_database_password", //This should be changed to reflect the password you used when setting up Postgres
   port: 5432,
 });
 
@@ -14,11 +14,11 @@ const pool = new Pool({
  */
 async function createTable() {
   // TODO: Add code to create Movies, Customers, and Rentals tables
-};
+}
 
 /**
  * Inserts a new movie into the Movies table.
- * 
+ *
  * @param {string} title Title of the movie
  * @param {number} year Year the movie was released
  * @param {string} genre Genre of the movie
@@ -26,43 +26,43 @@ async function createTable() {
  */
 async function insertMovie(title, year, genre, director) {
   // TODO: Add code to insert a new movie into the Movies table
-};
+}
 
 /**
  * Prints all movies in the database to the console
  */
 async function displayMovies() {
   // TODO: Add code to retrieve and print all movies from the Movies table
-};
+}
 
 /**
  * Updates a customer's email address.
- * 
+ *
  * @param {number} customerId ID of the customer
  * @param {string} newEmail New email address of the customer
  */
 async function updateCustomerEmail(customerId, newEmail) {
   // TODO: Add code to update a customer's email address
-};
+}
 
 /**
  * Removes a customer from the database along with their rental history.
- * 
+ *
  * @param {number} customerId ID of the customer to remove
  */
 async function removeCustomer(customerId) {
   // TODO: Add code to remove a customer and their rental history
-};
+}
 
 /**
  * Prints a help message to the console
  */
 function printHelp() {
-  console.log('Usage:');
-  console.log('  insert <title> <year> <genre> <director> - Insert a movie');
-  console.log('  show - Show all movies');
-  console.log('  update <customer_id> <new_email> - Update a customer\'s email');
-  console.log('  remove <customer_id> - Remove a customer from the database');
+  console.log("Usage:");
+  console.log("  insert <title> <year> <genre> <director> - Insert a movie");
+  console.log("  show - Show all movies");
+  console.log("  update <customer_id> <new_email> - Update a customer's email");
+  console.log("  remove <customer_id> - Remove a customer from the database");
 }
 
 /**
@@ -73,24 +73,24 @@ async function runCLI() {
 
   const args = process.argv.slice(2);
   switch (args[0]) {
-    case 'insert':
+    case "insert":
       if (args.length !== 5) {
         printHelp();
         return;
       }
       await insertMovie(args[1], parseInt(args[2]), args[3], args[4]);
       break;
-    case 'show':
+    case "show":
       await displayMovies();
       break;
-    case 'update':
+    case "update":
       if (args.length !== 3) {
         printHelp();
         return;
       }
       await updateCustomerEmail(parseInt(args[1]), args[2]);
       break;
-    case 'remove':
+    case "remove":
       if (args.length !== 2) {
         printHelp();
         return;
@@ -101,6 +101,6 @@ async function runCLI() {
       printHelp();
       break;
   }
-};
+}
 
 runCLI();
